aaac595af6551d373f8456732fc155db8ac8eaef
Tommy <tommyubongabasijohn@gmail.com>
2023-07-25 02:29:06 +0100

    ran lint command, made a few comments and used inbuilt config module

--- project-backend/src/app.module.ts
+++ project-backend/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user/user.entity';
 import { UserModule } from './modules/users.module';
-import { configDotenv } from 'dotenv';
-configDotenv();
+import { ConfigModule } from '@nestjs/config';
 
 Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }), //Provides global access to environment variables
     TypeOrmModule.forRoot({
       host: 'localhost',
       username: process.env.DB_USER,
@@ -16,11 +14,9 @@ Module({
       port: 3000,
       database: process.env.DB_DATABASE,
       type: 'postgres',
-      entities: [User],
+      entities: [User], //You can add a path to your entities here, take a look at the typeorm docs for more info. This will prevent manual addition of entities in the future.
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 });
 export class AppModule {}
