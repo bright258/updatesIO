8af7ca4d26938bbe580ad94bc7e442d019a725fd
Tommy <tommyubongabasijohn@gmail.com>
2023-07-27 06:55:02 +0100

    improved dtos

--- project-backend/src/DTOs/userDTO/logIn.DTO.ts
+++ project-backend/src/DTOs/userDTO/logIn.DTO.ts
@@ -1,11 +1,9 @@
 import { IsNotEmpty, MinLength, MaxLength } from 'class-validator';
 
-export class SignUpDTO {
-  @IsNotEmpty()
+export class LoginDto {
   @MaxLength(10)
-  @MinLength(4)
-  @MaxLength(20, { message: 'too long' })
-  @MinLength(4, { message: 'too short' })
+  @MaxLength(20, { message: 'Incorrect username or password' }) //I do not know if it is 10 or 20 you want so I cannot say which one is correct
+  @MinLength(4, { message: 'Incorrect username or password' })
   username: string;
 
   @IsNotEmpty()
--- project-backend/src/DTOs/userDTO/signUp.DTO.ts
+++ project-backend/src/DTOs/userDTO/signUp.DTO.ts
@@ -3,33 +3,28 @@ import {
   IsNotEmpty,
   MinLength,
   MaxLength,
-  Matches,
   IsStrongPassword,
+  Matches,
 } from 'class-validator';
 
 export class SignUpDTO {
-  @IsNotEmpty()
   @MaxLength(20)
   @MinLength(1)
   firstName: string;
 
-  @IsNotEmpty()
   @MaxLength(20)
   @MinLength(1)
   lastName: string;
 
-  @IsNotEmpty()
   @MaxLength(10, { message: 'WTF, bro. Find a shorter username' })
   @MinLength(4, { message: 'well... get something a bit longer' })
   username: string;
 
-  @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @IsNotEmpty()
-  @MaxLength(20, { message: 'too long' })
-  @MinLength(4, { message: 'too short' })
+  @MaxLength(20, { message: 'Password is too long' })
+  @MinLength(4, { message: 'Password is too short' })
   @IsStrongPassword()
   @Matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,16}$/, {
     message: `Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, and one underscore, and it 
@@ -39,13 +34,4 @@ export class SignUpDTO {
 
   @IsNotEmpty()
   dateOfBirth: Date;
-
-  @IsNotEmpty()
-  isActive: boolean;
-
-  @IsNotEmpty()
-  location: string;
-
-  @IsNotEmpty()
-  rank: string;
 }
